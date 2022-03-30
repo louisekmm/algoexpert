e29b314f4723358bbd862417cd3409bc771026bb
louisekmm <louisekmm@gmail.com>
2022-03-29 21:06:59 -0400

    resolution ex 2

--- two/ex2.py
+++ two/ex2.py
@@ -1,1 +1,20 @@
-teste
+def isValidSubsequence(array, sequence):
+	is_sequence = True
+	qty_equal = 0
+    # Write your code here.
+	print(array)
+	print(sequence)
+	s_aux = sequence
+	if len(sequence) > len(array):
+		return False
+
+	for s in sequence:
+		for i in array:
+			if s == i:
+				qty_equal += 1
+				s_aux.remove()
+
+	if qty_equal == len(sequence):
+		return True
+
+	return False
